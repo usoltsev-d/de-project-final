b2ef18d216c803bb5ee85bb3032569d58a0a101d
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-21 11:08:14 +0500

    v0.15

--- src/sql/ddl/raw/01_transaction_service_events.sql
+++ src/sql/ddl/raw/01_transaction_service_events.sql
@@ -3,9 +3,10 @@ CREATE TABLE IF NOT EXISTS raw.transaction_service_events
     object_id UUID,
     object_type LowCardinality(String),
     sent_dttm DateTime64(3, 'UTC'),
+    event_dttm DateTime64(3, 'UTC'),
     payload String,
     created_at DateTime64(3, 'UTC') DEFAULT now64(3)
 )
 ENGINE = MergeTree
-PARTITION BY toYYYYMMDD(sent_dttm)
-ORDER BY (sent_dttm, object_type, object_id);
+PARTITION BY toYYYYMMDD(event_dttm)
+ORDER BY (event_dttm, object_type, object_id);
