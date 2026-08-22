77e5e17e9769d5c39e6549449f71f2db40f6448d
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-22 10:31:05 +0500

    Revert "v0.22"
    This reverts commit 03e367ff45311841db46a0335692cfcc7c7e738b.

--- src/sql/ddl/stg/02_currencies.sql
+++ src/sql/ddl/stg/02_currencies.sql
@@ -3,9 +3,12 @@ CREATE TABLE IF NOT EXISTS stg.currencies
     date_update DateTime64(3, 'UTC'),
     currency_code UInt32,
     currency_code_with UInt32,
-    currency_with_div Decimal64(8),
-    load_dttm DateTime64(3, 'UTC') DEFAULT now64(3)
+    currency_with_div Decimal64(8)
 )
 ENGINE = MergeTree
 PARTITION BY toYYYYMMDD(date_update)
-ORDER BY date_update;
+ORDER BY (
+    date_update,
+    currency_code,
+    currency_code_with
+);
