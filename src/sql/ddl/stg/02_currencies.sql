8a7e374a15dc1af09cdf95a9f076d9bde0c3c5c2
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-22 09:56:50 +0500

    v0.22

--- src/sql/ddl/stg/02_currencies.sql
+++ src/sql/ddl/stg/02_currencies.sql
@@ -3,12 +3,9 @@ CREATE TABLE IF NOT EXISTS stg.currencies
     date_update DateTime64(3, 'UTC'),
     currency_code UInt32,
     currency_code_with UInt32,
-    currency_with_div Decimal64(8)
+    currency_with_div Decimal64(8),
+    load_dttm DateTime64(3, 'UTC') DEFAULT now64(3)
 )
 ENGINE = MergeTree
 PARTITION BY toYYYYMMDD(date_update)
-ORDER BY (
-    date_update,
-    currency_code,
-    currency_code_with
-);
+ORDER BY date_update;
