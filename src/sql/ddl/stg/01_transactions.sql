34c63247799be22e93a0cabd4948b14316498107
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-24 23:37:22 +0500

    v1.30

--- src/sql/ddl/stg/01_transactions.sql
+++ src/sql/ddl/stg/01_transactions.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS stg.transactions
 (
     operation_id UUID,
-    account_number_from UInt64,
-    account_number_to UInt64,
-    currency_code UInt32,
+    account_number_from Int64,
+    account_number_to Int64,
+    currency_code Int32,
     country LowCardinality(String),
     status LowCardinality(String),
     transaction_type LowCardinality(String),
-    amount UInt64,
+    amount Int64,
     transaction_dt DateTime64(3, 'UTC'),
     load_dttm DateTime64(3, 'UTC') DEFAULT now64(3)
 )
