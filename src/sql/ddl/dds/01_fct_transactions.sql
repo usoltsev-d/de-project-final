d0f1cb2d417afaf0797ef9a05af72efa43a541d4
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-25 09:06:43 +0500

    v1.33

---
+++ src/sql/ddl/dds/01_fct_transactions.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS stg.fct_transactions
+(
+    operation_id UUID,
+    account_number_from Int64,
+    account_number_to Int64,
+    currency_code Int32,
+    country LowCardinality(String),
+    status LowCardinality(String),
+    transaction_type LowCardinality(String),
+    amount Int64,
+    transaction_dt DateTime64(3, 'UTC'),
+    load_dttm DateTime64(3, 'UTC') DEFAULT now64(3)
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMMDD(transaction_dt)
+ORDER BY (
+    transaction_dt,
+    operation_id
+);
