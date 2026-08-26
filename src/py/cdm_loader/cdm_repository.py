a8d91fed24d75defd8e590b070fb43c9204c449d
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-26 18:43:51 +0500

    v1.49

---
+++ src/py/cdm_loader/cdm_repository.py
@@ -0,0 +1,70 @@
+from datetime import date
+from pathlib import Path
+
+from lib.clickhouse_client import ClickHouseClient
+
+
+class CdmRepository:
+    def __init__(
+        self,
+        clickhouse: ClickHouseClient,
+        sql_dir: Path,
+    ) -> None:
+        self._clickhouse = clickhouse
+
+        self._global_metrics_sql = (
+            sql_dir / "load_global_metrics_shadow.sql"
+        ).read_text(encoding="utf-8")
+
+    def drop_global_metrics_shadow_partition(
+        self,
+        process_date: date,
+    ) -> None:
+        partition_id = self._get_partition_id(process_date)
+
+        self._clickhouse.client.command(
+            """
+            ALTER TABLE cdm.global_metrics_shadow
+            DROP PARTITION {partition_id:UInt32}
+            """,
+            parameters={
+                "partition_id": partition_id,
+            },
+        )
+
+    def load_global_metrics_shadow_partition(
+        self,
+        process_date: date,
+    ) -> None:
+        self._clickhouse.client.command(
+            self._global_metrics_sql,
+            parameters={
+                "process_date": process_date,
+            },
+        )
+
+    def replace_global_metrics_partition(
+        self,
+        process_date: date,
+    ) -> None:
+        partition_id = self._get_partition_id(process_date)
+
+        self._clickhouse.client.command(
+            """
+            ALTER TABLE cdm.global_metrics
+            REPLACE PARTITION {partition_id:UInt32}
+            FROM cdm.global_metrics_shadow
+            """,
+            parameters={
+                "partition_id": partition_id,
+            },
+        )
+
+    def _get_partition_id(
+        self,
+        process_date: date,
+    ) -> int:
+        return int(process_date.strftime("%Y%m%d"))
+
+    def close(self) -> None:
+        self._clickhouse.close()
