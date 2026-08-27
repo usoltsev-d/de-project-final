f723b15c09023ec0a8ac619d9d68361d7e8cf6c9
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-27 22:40:52 +0500

    v1.68

---
+++ src/dags/raw_to_stg.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+
+from airflow.decorators import dag, task
+from airflow.models import Variable
+
+from stg_loader.load_currencies import main as load_currencies
+from stg_loader.load_transactions import main as load_transactions
+
+
+@dag(
+    dag_id="raw_to_stg",
+    schedule="* * * * *",
+    start_date=datetime(2022, 10, 1),
+    catchup=False,
+    max_active_runs=1,
+)
+def raw_to_stg():
+
+    @task
+    def load_transactions_task() -> None:
+        offset_from = int(
+            Variable.get(
+                "stg_transactions_last_offset",
+                default_var="-1",
+            )
+        )
+
+        batch_size = int(
+            Variable.get(
+                "stg_batch_size",
+                default_var="10000",
+            )
+        )
+
+        offset_to = load_transactions(
+            offset_from=offset_from,
+            batch_size=batch_size,
+        )
+
+        Variable.set(
+            "stg_transactions_last_offset",
+            offset_to,
+        )
+
+    @task
+    def load_currencies_task() -> None:
+        offset_from = int(
+            Variable.get(
+                "stg_currencies_last_offset",
+                default_var="-1",
+            )
+        )
+
+        batch_size = int(
+            Variable.get(
+                "stg_batch_size",
+                default_var="10000",
+            )
+        )
+
+        offset_to = load_currencies(
+            offset_from=offset_from,
+            batch_size=batch_size,
+        )
+
+        Variable.set(
+            "stg_currencies_last_offset",
+            offset_to,
+        )
+
+    load_transactions_task()
+    load_currencies_task()
+
+
+raw_to_stg()
