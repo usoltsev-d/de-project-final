a535d235d4ddb121a6050891fc808fa422ec11a5
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-08-30 17:58:28 +0500

    vv1.85

--- dags/dds_to_cdm.py
+++ dags/dds_to_cdm.py
@@ -4,6 +4,7 @@ from airflow.decorators import dag, task
 from airflow.models import Variable
 from airflow.sensors.external_task import ExternalTaskSensor
 
+from cdm_loader.check_missing_usd_rates import check_missing_usd_rates
 from cdm_loader.load_cdm_global_metrics import load_cdm_global_metrics
 
 
@@ -52,9 +53,19 @@ def dds_to_cdm():
             ).date()
         )
 
+    @task
+    def check_missing_usd_rates_task(
+        process_date: str,
+    ) -> None:
+        check_missing_usd_rates(
+            process_date=datetime.fromisoformat(
+                process_date
+            ).date()
+        )
+
     process_date = "{{ data_interval_start | ds }}"
 
-    wait_for_dds >> load_global_metrics_task(process_date)
+    wait_for_dds >> load_global_metrics_task(process_date) >> check_missing_usd_rates_task(process_date)
 
 
 dds_to_cdm()
--- src/py/cdm_loader/check_missing_usd_rates.py
+++ src/py/cdm_loader/check_missing_usd_rates.py
@@ -4,19 +4,13 @@ from pathlib import Path
 
 from airflow.hooks.base import BaseHook
 
-from cdm_loader.cdm_processor import CdmProcessor
 from cdm_loader.cdm_repository import CdmRepository
 from lib.clickhouse_client import ClickHouseClient
 
 
-def load_cdm_global_metrics(
+def check_cdm_usd_rates(
     process_date: date,
 ) -> None:
-
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(message)s",
-    )
 
     conn = BaseHook.get_connection("clickhouse_conn")
     extra = conn.extra_dejson
@@ -38,17 +32,20 @@ def load_cdm_global_metrics(
         sql_dir=sql_dir,
     )
 
-    processor = CdmProcessor(
-        drop_shadow_partition=repository.drop_global_metrics_shadow_partition,
-        load_shadow_partition=repository.load_global_metrics_shadow_partition,
-        replace_partition=repository.replace_global_metrics_partition,
-    )
-
     try:
-        processor.run(process_date)
+        # Проверяем, что для всех валют с успешными транзакциями доступны прямые курсы пересчёта в USD.
+        missing_rates = repository.get_missing_usd_rates(
+            process_date
+        )
+
+        if missing_rates:
+            raise ValueError(
+                f"Missing USD rates for date={process_date}: "
+                f"currency_from={missing_rates}"
+            )
 
         logging.info(
-            "CDM global_metrics processed for date=%s",
+            "USD rates validation passed for date=%s",
             process_date,
         )
     finally:
