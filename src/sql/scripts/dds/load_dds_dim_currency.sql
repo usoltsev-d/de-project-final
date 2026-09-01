504e95098417a53ae24f12c7e7d477ac49f66e5e
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-09-01 19:01:08 +0500

    v2.15

--- src/sql/scripts/dds/load_dds_dim_currency.sql
+++ src/sql/scripts/dds/load_dds_dim_currency.sql
@@ -1,11 +1,22 @@
 INSERT INTO dds.dim_currency
 (
     currency_code,
-    country
+    country,
+    currency_iso_code
 )
 SELECT DISTINCT
     currency_code,
-    country
+    country,
+    CASE country
+        WHEN 'canada'  THEN 'CAD'
+        WHEN 'usa'     THEN 'USD'
+        WHEN 'russia'  THEN 'RUB'
+        WHEN 'italy'   THEN 'EUR'
+        WHEN 'china'   THEN 'CNY'
+        WHEN 'england' THEN 'GBP'
+        WHEN 'turkey'  THEN 'TRY'
+        ELSE 'UNKNOWN'
+    END AS currency_iso_code
 FROM dds.transactions
 WHERE transaction_dt >= toDateTime64({process_date:Date}, 3, 'UTC')
 AND transaction_dt <  toDateTime64({process_date:Date} + INTERVAL 1 DAY, 3, 'UTC')
