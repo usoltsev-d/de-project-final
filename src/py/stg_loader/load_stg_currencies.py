84d708172faa22230a85c63ea7816058526a9832
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-09-03 11:23:06 +0500

    v2.32

--- src/py/stg_loader/load_stg_currencies.py
+++ src/py/stg_loader/load_stg_currencies.py
@@ -24,16 +24,12 @@ def load_stg_currencies(
     kafka_partition = 0
 
     conn = BaseHook.get_connection("clickhouse_conn")
-    extra = conn.extra_dejson
 
     client = clickhouse_connect.get_client(
         host=conn.host,
         port=conn.port,
         username=conn.login,
         password=conn.password,
-        database=conn.schema or "raw",
-        secure=extra.get("secure", False),
-        ca_cert=extra.get("cert_path"),
     )
 
     sql_dir = Path("src/sql/scripts/stg")
