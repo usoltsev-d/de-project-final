e58bbad71f961804a6555ae8b2d6f167a9775f80
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-09-03 11:26:02 +0500

    v2.34

--- src/py/dds_loader/load_dds_dim_currency.py
+++ src/py/dds_loader/load_dds_dim_currency.py
@@ -18,16 +18,12 @@ def load_dds_dim_currency(
     )
 
     conn = BaseHook.get_connection("clickhouse_conn")
-    extra = conn.extra_dejson
 
     client = clickhouse_connect.get_client(
         host=conn.host,
         port=conn.port,
         username=conn.login,
         password=conn.password,
-        database=conn.schema or "dds",
-        secure=extra.get("secure", False),
-        ca_cert=extra.get("cert_path"),
     )
 
     sql_dir = Path("src/sql/scripts/dds")
