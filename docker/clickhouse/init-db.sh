6d6580b43b10bc188242107d745a8c1be4183585
Dmitry Usoltsev <knightofcups@yandex.ru>
2026-09-03 11:31:16 +0500

    v2.37

--- docker/clickhouse/init-db.sh
+++ docker/clickhouse/init-db.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Creating databases..."
-
-clickhouse-client \
-  --user "$CLICKHOUSE_USER" \
-  --password "$CLICKHOUSE_PASSWORD" \
-  --multiquery <<SQL
-
-CREATE DATABASE IF NOT EXISTS raw;
-CREATE DATABASE IF NOT EXISTS stg;
-CREATE DATABASE IF NOT EXISTS dds;
-CREATE DATABASE IF NOT EXISTS cdm;
-
-SQL
-
-
 apply_ddl() {
     directory="$1"
 
